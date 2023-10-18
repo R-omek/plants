ef2a62b04d4d9912fd5f8d57d5ffaf8518f26d7c
roman hrytsiuk <rmngrck97@gmail.com>
2023-10-18 18:46:28 +0300

    configure webpack, add header

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  entry: {
+    app: './src/index.ts',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext]'
+  },
+  devServer: develop 
+  ? {
+    open: true,
+    hot: true,
+    port: 4200,
+    static: { 
+      directory: path.join(__dirname, 'public') 
+    },
+    watchFiles: ['src/**/*']
+  } : {},
+  module: {
+    rules: [
+      {
+        test: /\.[ts]s$/,
+        use: [ 'ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|pnj|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?ico|gif|otf|ttf|eot|svg)$/i,
+        type: 'asset/inline'
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Demo webpack',
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: develop ? '[name].css' : '[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: './public'}
+      ]
+    }),
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+  ],
+});
