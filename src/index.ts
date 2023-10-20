eaa3cd11956752d38d3b8030a02a8323de9c030a
roman hrytsiuk <rmngrck97@gmail.com>
2023-10-20 20:05:11 +0300

    add projects animations

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,42 @@ import { addFavicon } from './scripts/add-favicon'
 
 addFavicon()
 
-const projects = {
-    garden: [
-        {name: 'Garden care', description: 'Lorem Ipsum has been the industry', img: require('./assets/garden/1.png')},
-        {name: 'Planting', description: 'Lorem Ipsum has been the industry', img: require('./assets/garden/2.png')},
-        {name: 'Lawn care', description: 'Lorem Ipsum has been the industry', img: require('./assets/garden/3.png')},
-        {name: 'Planting', description: 'Lorem Ipsum has been the industry', img: require('./assets/garden/4.png')},
-        {name: 'Garden care', description: 'Lorem Ipsum has been the industry', img: require('./assets/garden/5.png')},
-        {name: 'Planting', description: 'Lorem Ipsum has been the industry', img: require('./assets/garden/6.png')},
-    ]
+
+const PROJECTS_BTNS = document.querySelectorAll('.projects__btn')
+const PROJECTS_ITEMS: NodeListOf<HTMLDivElement> = document.querySelectorAll('.projects__item')
+
+PROJECTS_BTNS.forEach(btn => {
+    btn.addEventListener('click', function(this: HTMLButtonElement) {
+        blurProjects(this)
+    })
+})
+
+function blurProjects(target: HTMLButtonElement) {
+    PROJECTS_ITEMS.forEach(item => {
+        const isGarden = item.children[1].textContent?.split(' ')[0] === 'Garden'
+        const isSameProject = item.children[1].textContent?.split(' ')[0].toLowerCase() === target.textContent?.toLowerCase()
+        item.classList.remove('blur-scale')
+        toggleProjectsBtn(target)
+        if (target.textContent === 'Gardens') {
+            if (!isGarden) {
+                item.classList.add('blur-scale')
+            }
+        } else {
+            item.classList.remove('blur-scale')
+            if (!isSameProject) {
+                item.classList.add('blur-scale')
+            }
+        }
+
+        item.addEventListener('click', resetProjectItem)
+    })
 }
 
-const PROJECT_IMG: NodeListOf<HTMLImageElement> = document.querySelectorAll('.projects__img')
-const PROJECT_NAME = document.querySelectorAll('.projects__name')
-const PROJECT_DESC = document.querySelectorAll('.projects__desc')
+function resetProjectItem(this: HTMLDivElement) {
+    this.classList.remove('blur-scale')
+}
 
-projects.garden.forEach((project, index) => {
-    PROJECT_IMG[index].src = project.img
-    PROJECT_NAME[index].innerHTML = project.name
-    PROJECT_DESC[index].innerHTML = project.description
-})
+function toggleProjectsBtn(target: HTMLButtonElement) {
+    PROJECTS_BTNS.forEach(btn => btn.classList.remove('active'))
+    target.classList.add('active')
+}
